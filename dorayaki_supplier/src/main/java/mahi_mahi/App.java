e625fd76bce2234804cef88f5fe3451249bed2b8
Reinaldo16519320 <sandyshroud00@gmail.com>
2021-11-17 00:33:35 +0800

    Init maven project

---
+++ dorayaki_supplier/src/main/java/mahi_mahi/App.java
@@ -0,0 +1,13 @@
+package mahi_mahi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
